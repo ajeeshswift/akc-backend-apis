82c9c8b8cdd7dc48fd2bc9a3a1ad3f095225e458
Sathish <sathish.2009btech@gmail.com>
2021-02-17 14:22:46 +0530

    Initial Commit

---
+++ src/main/java/com/swift/akc/AKCApplication.java
@@ -0,0 +1,12 @@
+package com.swift.akc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AKCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AKCApplication.class, args);
+    }
+}
